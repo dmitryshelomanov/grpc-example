9cc25707a9dbdc2c7c904080bd5254cab7dd519f
Dmitry Shelomanov <userbq201@mail.ru>
2019-05-21 14:14:34 +0300

    refactore code style & add method for book

--- client.js
+++ client.js
@@ -6,8 +6,37 @@ const booksDefinition = protoLoader.loadSync('books.proto', {})
 const booksProto = grpc.loadPackageDefinition(booksDefinition)
 const client = new booksProto.books.BookService('localhost:50051', grpc.credentials.createInsecure())
 
-client.insert({ title: Math.random().toString(), author: Math.random().toString() }, function(err, response) {
-  client.list({}, function(err, response) {
-    console.log(response)
-  })  
+const buildBook = () => ({
+  id: process.argv[3] || 1,
+  title: Math.random().toString(),
+  author: Math.random().toString(),
 })
+
+const routes = {
+  getBooks() {
+    client.list({}, (err, response) => {
+      console.log(response)
+    })
+  },
+  addBook() {
+    client.insert(buildBook(), (err, response) => {
+      this.getBooks()
+    })
+  },
+  getBook() {
+    const id = process.argv[3] || 1
+
+    client.get({ id }, (err, response) => {
+      if (!err) {
+        console.log(response)
+      }
+      else {
+        throw new Error(err)
+      }
+    })
+  },
+}
+
+const method = process.argv[2] || 'getBooks'
+
+routes[method]()
--- server.js
+++ server.js
@@ -8,12 +8,25 @@ const server = new grpc.Server()
 const books = []
 
 server.addService(booksProto.books.BookService.service, {
-  list: function (call, callback) {
+  list(call, callback) {
     callback(null, { books })
   },
-  insert: function (call, callback) {
+  insert(call, callback) {
     books.push(call.request)
     callback(null, {})
+  },
+  get(call, callback) {
+    const book = books.find((b) => b.id === call.request.id)
+
+    if (book) {
+      callback(null, book)
+    }
+    else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        message: 'Not Found',
+      })
+    }
   },
 })
 server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
